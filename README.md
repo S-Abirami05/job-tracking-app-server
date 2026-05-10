95b14c2b4dad3cb3326f57e50f0ae07b5caba5c6
Abirami <abiramikevin@gmail.com>
2026-05-10 14:03:50 +0530

    chore: initialize package.json with dependencies for job tracking app server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # job-tracking-app-server
+
+#install packages
+#1.npm init --y
+#2.npm install express dotenv mongoose cors nodemon
---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
